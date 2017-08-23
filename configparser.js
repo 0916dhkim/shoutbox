4d930d9cb9af7a4124ff98b44b5638db0fda2332
Donghyeon Kim <donghyeon@tacocube.com>
2017-08-22 22:21:41 -0400

    Add Default Config File
    Use default config file and only override values if given in custom config
    file.

--- configparser.js
+++ configparser.js
@@ -1,4 +1,47 @@
 const fs = require('fs');
 const configPath = 'shoutbox.config.json';
+const defaultConfigPath = configPath + '.default';
 
-module.exports = JSON.parse(fs.readFileSync(configPath));
+var ret = {};
+
+try {
+    // Check read access to default config file.
+    fs.accessSync(defaultConfigPath, fs.constants.R_OK);
+
+    // Read default config file.
+    var defaultConfig = JSON.parse(fs.readFileSync(defaultConfigPath));
+
+    var customConfig = undefined;
+    try {
+        // Check read access to custom config file.
+        fs.accessSync(configPath, fs.constants.R_OK);
+
+        // Read custom config file.
+        customConfig = JSON.parse(fs.readFileSync(configPath));
+
+        // Override with custom config.
+        for (var conf in defaultConfig) {
+            if (customConfig[conf] === undefined) {
+                // If there is no custom config,
+                // use default value.
+                ret[conf] = defaultConfig[conf];
+            } else {
+                // If there is custom config,
+                // use custom value.
+                ret[conf] = customConfig[conf];
+            }
+        }
+    } catch (e) {
+        // If there is no custom config file,
+        // use default.
+        ret = defaultConfig;
+    }
+
+} catch (e) {
+    // If there is no default config file,
+    console.log('Default configuration read fail');
+    // Exit.
+    process.exit(1);
+}
+
+module.exports = ret;
